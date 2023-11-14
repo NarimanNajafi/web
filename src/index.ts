3cc518b9baaf3d0e37688b7587f2315ea30727a2
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-14 16:58:39 +0330

    index updated

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Pasha', age: 25 });
 
-user.set({ name: 'newName', age: 46 });
+// user.set({ name: 'newName' });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+
+user.on('change', () => {});
+console.log(user);
 
 // console.log('hi there!');
