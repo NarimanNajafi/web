4f4d1ee92e99477f31b36f533bc3a5dce82dfb2c
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-16 12:01:11 +0330

    user updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,15 @@
 import { Eventhing } from './Eventing';
+import { Sync } from './Sync';
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/user';
 export class User {
   public events: Eventhing = new Eventhing();
+  public sync: Sync<UserProps> = new Sync<UserProps>();
 
   constructor(private data: UserProps) {}
 
