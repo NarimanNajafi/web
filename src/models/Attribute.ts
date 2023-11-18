3a28edf6f9fd06b2ea58cff66d2103e920751fbb
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-18 21:08:44 +0330

    attribute refactored

--- src/models/Attribute.ts
+++ src/models/Attribute.ts
@@ -2,15 +2,11 @@ import { User, UserProps } from './User';
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string | boolean {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
-
-const attrs = new Attributes<UserProps>({ id: 5, name: 'anything' });
-
-const id = attrs.get('id');
