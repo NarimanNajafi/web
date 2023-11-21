9e2eddcff0e1375d9d37b982551fb20e77518242
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-21 10:46:20 +0330

    big fixed!

--- src/models/Attribute.ts
+++ src/models/Attribute.ts
@@ -2,9 +2,9 @@ import { User, UserProps } from './User';
 export class Attributes<T extends object> {
   constructor(private data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     Object.assign(this.data, update);
