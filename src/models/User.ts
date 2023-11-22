8c9b2ba6429e405eb4d44b6db281afa1a6ac337b
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-22 12:24:51 +0330

    user updated!

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import { Attributes } from './Attribute';
 import { Model } from './Models';
+import { Eventing } from './Eventing';
+import { ApiSync } from './ApiSync';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -6,4 +9,12 @@ export interface UserProps {
 }
 
 const rootUrl = 'http://localhost:3000/users';
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
