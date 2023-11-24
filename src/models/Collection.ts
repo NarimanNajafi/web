3192d3a4070d7fa3646ef4d7f32ac6d8a1b3ed6c
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-11-24 17:23:13 +0330

    index updated!

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -17,8 +17,7 @@ export class Collection<T, K> {
 
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
-      response.data.forEach((value: k) => {
-        const user = User.buildUser(value);
+      response.data.forEach((value: K) => {
         this.models.push(this.deserialize(value));
       });
 
